ff75736058f26161d6f589cecb4045687dd8bf0b
shayan <shayan@UNKNOWN>
2023-05-28 13:16:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("hello dude");
+        
+        Console.WriteLine("enter a number between 1-10: ");
+        int number;
+        if(int.TryParse(Console.ReadLine(),out number))
+        {
+            if(number >=1 && number <= 10)
+            {
+                Console.WriteLine("valid");
+
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+            
+
+        }
+        else
+        {
+            Console.WriteLine("invalid");
+        }
+
+    }
+}
